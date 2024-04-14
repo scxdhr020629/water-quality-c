35fdd0564e0ccb18fa03544cffd0ccc3a4fcf446
鹤纸千 <75164643+scxdhr020629@users.noreply.github.com>
2024-04-14 16:31:19 +0800

    PH 和 TDS

--- iot.c
+++ iot.c
@@ -29,6 +29,18 @@ float temp_data = 25.0;
 // 这个温度应该是要被读取的 但是默认现在是室温环境好了
 float K_Value = 3347.19;
 
+// PH 所需要的参数
+#define SensorPin 36            //pH meter Analog output to Arduino Analog Input 2
+#define Offset 12.88           //deviation compensate
+// #define LED 13
+#define samplingInterval 20
+#define printInterval 800
+#define ArrayLenth  40    //times of collection
+int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
+int pHArrayIndex=0;  
+
+
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 
@@ -91,7 +103,10 @@ void loop()
     float TU = sensorValue * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
     TU_calibration = -0.0192 * (temp_data - 25) + TU;
     TU_value = -865.68 * TU_calibration + K_Value;
-
+    Serial.print("SensorValue ");
+    Serial.println(sensorValue);
+    Serial.print("TU ");
+    Serial.println(TU);
     if (TU_value <= 0)
     {
         TU_value = 0;
@@ -107,14 +122,55 @@ void loop()
     // 这里把 ph 写死先
     float PH = 7.0;
 
+
+
+    // ph 数据 获取
+    static unsigned long samplingTime = millis();
+    static unsigned long printTime = millis();
+    static float pHValue, voltage;
+    if (millis() - samplingTime > samplingInterval)
+    {
+        pHArray[pHArrayIndex++] = analogRead(SensorPin);
+        if (pHArrayIndex == ArrayLenth)
+            pHArrayIndex = 0;
+        voltage = avergearray(pHArray, ArrayLenth) * 5.0 / 1024;
+        pHValue = -0.6897 * voltage + Offset;
+        if (pHValue <= 0.0)
+        {
+            pHValue = 0.0;
+        }
+        if (pHValue > 14.0)
+        {
+            pHValue = 14.0;
+        }
+        samplingTime = millis();
+    }
+    // 线性拟合了 pHValue 
+
+    PH = pHValue;
+
+
+
+    // json 数据 发送 mqtt
     doc["TDS"] = TDS;
     doc["PH"] = PH;
 
+    // 串口打印 TDS
     Serial.print("TU Value:");
     Serial.println(TU_value); // print out the value you read:
-    Serial.print("PH:");
-    Serial.println(PH);
-    
+    // 串口打印PH 
+    // Serial.print("PH:");
+    // Serial.println(PH);
+    if (millis() - printTime > printInterval) // Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+    {
+        Serial.print("Voltage:");
+        Serial.print(voltage, 2);
+        Serial.print("    pH value: ");
+        Serial.println(pHValue, 2);
+        // digitalWrite(LED,digitalRead(LED)^1);
+        printTime = millis();
+    }
+
     char buffer[256];
     size_t n = serializeJson(doc, buffer);
     client.publish(topic, buffer, n);
@@ -123,4 +179,63 @@ void loop()
     // client.publish(topic,TUString);
     delay(1000);
     
+}
+
+
+// PH 参数处理
+double avergearray(int *arr, int number)
+{
+    int i;
+    int max, min;
+    double avg;
+    long amount = 0;
+    if (number <= 0)
+    {
+        Serial.println("Error number for the array to avraging!/n");
+        return 0;
+    }
+    if (number < 5)
+    { // less than 5, calculated directly statistics
+        for (i = 0; i < number; i++)
+        {
+            amount += arr[i];
+        }
+        avg = amount / number;
+        return avg;
+    }
+    else
+    {
+        if (arr[0] < arr[1])
+        {
+            min = arr[0];
+            max = arr[1];
+        }
+        else
+        {
+            min = arr[1];
+            max = arr[0];
+        }
+        for (i = 2; i < number; i++)
+        {
+            if (arr[i] < min)
+            {
+                amount += min; // arr<min
+                min = arr[i];
+            }
+            else
+            {
+                if (arr[i] > max)
+                {
+                    amount += max; // arr>max
+                    max = arr[i];
+                }
+                else
+                {
+                    amount += arr[i]; // min<=arr<=max
+                }
+            } // if
+        }     // for
+        avg = (double)amount / (number - 2);
+    } // if
+    return avg;
 }
